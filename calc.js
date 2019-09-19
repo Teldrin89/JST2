f5a2c9c7a19976c545f6a6871cb4fdb409597f41
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-09-19 22:02:45 +0200

    set initial variables for calculator operations

--- calc.js
+++ calc.js
@@ -1,1 +1,13 @@
-// JS calculator app
+// JS calculator application based on 4 values that will be used for simple calculation operations: +, -, /, *
+// store of previous value from entry area
+let pevVal = "";
+// store new value that will be put after operation is selected
+let newVal = "";
+// store math operator
+let mathOperator = "";
+// store the result of calculation
+let resultVal = "";
+// error handling: track how many times user clicked on decimal and not allow for more than one click
+let decimalClicked = false;
+// memory stored value
+let valMemStored = "";
