911207db1347a0c8ce3ac8b70ba934be3c4d5bd6
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-09-19 22:18:50 +0200

    created the clear and memory functions

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
 // JS calculator application based on 4 values that will be used for simple calculation operations: +, -, /, *
 // store of previous value from entry area
-let pevVal = "";
+let prevVal = "";
 // store new value that will be put after operation is selected
 let newVal = "";
 // store math operator
@@ -29,15 +29,29 @@ function equalButPress(num){
 
 // function for clearing data from input area
 function clearButPress(){
-
+    // reset all of the variables
+    prevVal = "";
+    newVal = "";
+    resultVal = "";
+    mathOperator = "";
+    decimalClicked = false;
+    // set entry to "0"
+    document.getElementById("entry").value = "0";
 }
 
 // function for copying number to memory
 function copyButPress(num){
-
+    // store the entered value in variable
+    valMemStored = document.getElementById("entry").value;
 }
 
 // function to paste the number in memory
 function pasteButPress(num){
-
+    // if there is a value stored in memory
+    if(valMemStored){
+        // paste the stored value to entry space
+        document.getElementById("entry").value = valMemStored;
+        // set new value as the one stored
+        newVal = valMemStored;
+    }
 }
